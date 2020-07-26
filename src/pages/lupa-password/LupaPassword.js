9de3795394b992b8d69d6b2036bd774e855ab867
Muhammad Tegar Akmal <eategar@gmail.com>
2020-07-26 18:12:12 +0700

    finish set folder structure and react :art:

---
+++ src/pages/lupa-password/LupaPassword.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function lupaPassword() {
+    return (
+        <div>
+            <h1>Halaman Lupa Password</h1>
+        </div>
+    )
+}
+
+export default lupaPassword
